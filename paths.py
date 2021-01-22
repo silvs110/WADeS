4188f755cee46346a71e97eff8e0748fd496d4d5
Silvia <silviasiu1998@gmail.com>
2021-01-22 02:45:13 -0500

    Created methods to get latest retrieved data and last modelled data
    Closes #28.
    
    - Created methods to get the latest retrieved data and the last modelled data in AppProfile.
    - Added new method to retrieved saved application profiles as dictionaries.
    - Added unit tests for the new methods.

--- paths.py
+++ paths.py
@@ -3,3 +3,4 @@ import pathlib
 ROOT_PATH = pathlib.Path(__file__).parent.absolute()
 APP_PROF_DATA_PATH = ROOT_PATH / "data" / "app_profiles"
 APP_PROF_DATA_PATH.parent.mkdir(parents=True, exist_ok=True)
+SAMPLE_APP_PROF_DATA_PATH = ROOT_PATH / "src/tests/sample_data/sample_app_profile_data"
--- src/tests/test_app_profile_data_manager.py
+++ src/tests/test_app_profile_data_manager.py
@@ -1,20 +1,26 @@
 import pytest
 
+
 from src.main.AppProfile import AppProfile
 from src.main.AppProfileAttribute import AppProfileAttribute
 from src.main.AppProfileDataManager import AppProfileDataManager
 from src.main.ProcessHandler import ProcessHandler
+from paths import SAMPLE_APP_PROF_DATA_PATH
+from src.tests.test_app_profile import check_app_profile_has_the_right_format
+
 
 """
 Contains tests for AppProfileDataManager class
 
 Functional tests:
 * save_app_profiles()
-* save_app_profiles()
+* get_saved_profiles()
+* get_saved_profiles_as_dict()
 
 Input Validation tests:
 * save_app_profiles()
-
+* get_saved_profiles()
+* get_saved_profiles_as_dict()
 """
 
 
@@ -22,26 +28,36 @@ Input Validation tests:
 def test_save_app_profiles_with_input_validation() -> None:
     """
     Test save_app_profiles() with input validation.
+    Checks that an exception is thrown when an invalid input is provided.
     """
-    # None type as input
+    # None input for app_profiles
     with pytest.raises(TypeError):
         AppProfileDataManager.save_app_profiles(app_profiles=None)
 
-    # Empty string as input
+    # Empty string input for app_profiles
     with pytest.raises(TypeError):
         AppProfileDataManager.save_app_profiles(app_profiles="")
 
-    # Invalid elements in list as input
+    # Invalid elements in list as input for app_profiles
     with pytest.raises(TypeError):
         AppProfileDataManager.save_app_profiles(app_profiles=["!2", "error"])
 
     with pytest.raises(TypeError):
         AppProfileDataManager.save_app_profiles(app_profiles=[2, 7])
 
+    # None input for app_profile_file
+    with pytest.raises(TypeError):
+        AppProfileDataManager.save_app_profiles(app_profiles=list(), app_profile_file=None)
+
+    # Invalid input type for app_profile_file
+    with pytest.raises(TypeError):
+        AppProfileDataManager.save_app_profiles(app_profiles=list(), app_profile_file=5)
+
 
 def test_save_and_get_profile_data() -> None:
     """
     Test save_app_profiles() and get_saved_profiles().
+    Checks that saving and retrieving the app profiles does not modify the data.
     """
     process_handler = ProcessHandler()
     process_handler.collect_running_processes_information()
@@ -69,3 +85,52 @@ def test_save_and_get_profile_data() -> None:
         assert expected_opened_files == actual_opened_files
 
         assert expected_app_profile_dict == actual_app_profile_dict
+
+
+def test_get_app_profile_as_dict_with_valid_input() -> None:
+    """
+    Test get_saved_profiles_as_dict().
+    Checks that get_saved_profiles_as_dict() can retrieve app_profiles as dict in the right format.
+    """
+    app_name = "common_case_app"
+    app_profiles = AppProfileDataManager.get_saved_profiles_as_dict(SAMPLE_APP_PROF_DATA_PATH)
+    app_profile_attribute_names = {enum.name for enum in AppProfileAttribute}
+
+    assert isinstance(app_profiles, dict)
+    assert len(app_profiles) > 0
+
+    app_profile = app_profiles[app_name]
+    actual_app_profile_attrs = app_profile.keys()
+    assert actual_app_profile_attrs == app_profile_attribute_names
+
+    check_app_profile_has_the_right_format(app_name, app_profile)
+
+
+# noinspection PyTypeChecker
+def test_get_app_profile_as_dict_with_invalid_input() -> None:
+    """
+    Test get_saved_profiles_as_dict() with invalid inputs.
+    Checks that an exception is thrown when an invalid input is provided.
+    """
+    # None input for app_profile_file
+    with pytest.raises(TypeError):
+        AppProfileDataManager.get_saved_profiles_as_dict(app_profile_file=None)
+
+    # Invalid input type for app_profile_file
+    with pytest.raises(TypeError):
+        AppProfileDataManager.get_saved_profiles_as_dict(app_profile_file=5)
+
+
+# noinspection PyTypeChecker
+def test_get_app_profile_with_invalid_input() -> None:
+    """
+    Test get_saved_profiles() with invalid inputs.
+    Checks that an exception is thrown when an invalid input is provided.
+    """
+    # None input for app_profile_file
+    with pytest.raises(TypeError):
+        AppProfileDataManager.get_saved_profiles(app_profile_file=None)
+
+    # Invalid input type for app_profile_file
+    with pytest.raises(TypeError):
+        AppProfileDataManager.get_saved_profiles(app_profile_file=5)
