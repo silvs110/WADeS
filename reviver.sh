5143470572d3fee2af4c3d62a6c21e5f1013e579
Silvia <silviasiu1998@gmail.com>
2021-02-01 20:11:47 -0500

    Added bash script that checks that all daemons are running
    Closes #49.
    Added bash script to check that all daemons are running, and if not rerun them.
    Added bash script to stop all running scripts and the daemon reviver.sh

---
+++ reviver.sh
@@ -0,0 +1,20 @@
+run_path="${HOME}/.wades/run"
+ps_handler_file_path="${run_path}/ProcessHandler.pid"
+modeller_file_path="${run_path}/Modeller.pid"
+
+echo $$ >> "${HOME}/.wades/run/wades_reviver.pid"
+
+while true
+  do
+    if [ ! -f "${ps_handler_file_path}" ]
+    then
+      echo ps_handler is not running. Rerunning it.
+      python3 ./pshandler_starter.py
+    fi
+
+    if [ ! -f "${modeller_file_path}" ]
+    then
+      echo modeller is not running. Rerunning it.
+      python3 ./modeller_starter.py
+    fi
+  done
